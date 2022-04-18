6b13d2a6e97beee1b7674f2f9007449bff3065c4
Dummy Bird <nilpoint@hotmail.com>
2022-04-18 13:15:40 +0800

    Create a minimum Spring Security project.

---
+++ src/test/java/com/topeid/springsecuritysample/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topeid.springsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
