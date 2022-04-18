7fa138e5e5c25145519a4da685f828a573021e9c
Dummy Bird <nilpoint@hotmail.com>
2022-04-18 13:23:09 +0800

    Add a home method for root URL path("/").

---
+++ src/main/java/com/topeid/springsecuritysample/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.topeid.springsecuritysample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  @GetMapping("/")
+  public String home() {
+    return "Welcome to Learn Spring Security.";
+  }
+}
